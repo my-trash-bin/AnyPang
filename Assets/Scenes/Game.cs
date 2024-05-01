28af35fa9460fe04396a11921988280f2f51c9dc
Juyeong Maing <mjy9088@naver.com>
2024-05-02 01:13:01 +0900

    add prefab

--- Assets/Scenes/Game.cs
+++ Assets/Scenes/Game.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class Game : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    Transform bluePrefab, redPrefab, greenPrefab, yellowPrefab, blackPrefab;
+
+    const int SIZE = 10;
+
+    static Vector3 Position(int x, int y)
     {
-        
+        Vector3 result;
+        result.y = y * 2 - 9;
+        result.x = x * 2 - 9;
+        result.z = 0;
+        return result;
     }
 
-    // Update is called once per frame
+    Transform[][] gemPool;
+
+    void Awake()
+    {
+        Transform[] gemType = { bluePrefab, redPrefab, greenPrefab, yellowPrefab, blackPrefab };
+        gemPool = new Transform[5][];
+        for (int i = 0; i < 5; i++)
+        {
+            gemPool[i] = new Transform[SIZE * SIZE];
+            for (int j = 0; j < SIZE * SIZE; j++)
+            {
+                Transform gem = gemPool[i][j] = Instantiate(gemType[i]);
+                gem.localPosition = Position(j % SIZE, j / SIZE);
+            }
+        }
+    }
+
+    void Start()
+    {
+
+    }
+
     void Update()
     {
-        
+
     }
 }
