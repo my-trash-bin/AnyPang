08c9afe49e1864094694aac2a5f09794a5d5de32
Juyeong Maing <mjy9088@naver.com>
2024-05-02 01:29:22 +0900

    add map initialization

--- Assets/Scenes/Game.cs
+++ Assets/Scenes/Game.cs
@@ -8,6 +8,7 @@ public class Game : MonoBehaviour
     Transform bluePrefab, redPrefab, greenPrefab, yellowPrefab, blackPrefab;
 
     const int SIZE = 10;
+    System.Random random = new();
 
     static Vector3 Position(int x, int y)
     {
@@ -20,6 +21,8 @@ public class Game : MonoBehaviour
 
     Transform[][] gemPool;
 
+    int[][] map;
+
     void Awake()
     {
         Transform[] gemType = { bluePrefab, redPrefab, greenPrefab, yellowPrefab, blackPrefab };
@@ -31,17 +34,28 @@ public class Game : MonoBehaviour
             {
                 Transform gem = gemPool[i][j] = Instantiate(gemType[i]);
                 gem.localPosition = Position(j % SIZE, j / SIZE);
+                gem.gameObject.SetActive(false);
             }
         }
     }
 
     void Start()
     {
-
+        map = new int[SIZE][];
+        for (int i = 0; i < SIZE; i++)
+        {
+            map[i] = new int[SIZE];
+            for (int j = 0; j < SIZE; j++)
+                map[i][j] = random.Next(5);
+        }
     }
 
     void Update()
     {
-
+        for (int i = 0; i < SIZE * SIZE; i++)
+        {
+            for (int j = 0; j < 5; j++)
+                gemPool[j][i].gameObject.SetActive(map[i / SIZE][i % SIZE] == j);
+        }
     }
 }
